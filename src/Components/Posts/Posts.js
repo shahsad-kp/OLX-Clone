c1d635b820952f0077dd7bc20c6baf9aae236672
shahsad <shahsadkpklr@gmail.com>
2023-05-29 20:18:56 +0530

    home page makes dynamic

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {collection, getDocs} from "firebase/firestore"
 
 import Heart from '../../assets/Heart';
 import './Post.css';
+import {FirebaseContext} from "../../store/FirebaseContext";
 
 function Posts() {
+    const [posts, setPosts] = useState([]);
+    const {firestore} = useContext(FirebaseContext);
+
+    useEffect(() => {
+        getDocs(
+            collection(firestore, 'products'),
+        ).then((snapshot) => {
+            setPosts(
+                snapshot.docs.map((product) => {
+                    return {
+                        ...product.data(),
+                        id: product.id
+                    }
+                })
+            )
+        })
+    }, []);
+
 
     return (
         <div className="postParentDiv">
@@ -13,24 +33,28 @@ function Posts() {
                     <span>View more</span>
                 </div>
                 <div className="cards">
-                    <div
-                        className="card"
-                    >
-                        <div className="favorite">
-                            <Heart></Heart>
-                        </div>
-                        <div className="image">
-                            <img src="../../../Images/R15V3.jpg" alt=""/>
-                        </div>
-                        <div className="content">
-                            <p className="rate">&#x20B9; 250000</p>
-                            <span className="kilometer">Two Wheeler</span>
-                            <p className="name"> YAMAHA R15V3</p>
-                        </div>
-                        <div className="date">
-                            <span>Tue May 04 2021</span>
-                        </div>
-                    </div>
+                    {
+                        posts.map((product) => {
+                            return (<div
+                                className="card"
+                            >
+                                <div className="favorite">
+                                    <Heart></Heart>
+                                </div>
+                                <div className="image">
+                                    <img src={product.image} alt=""/>
+                                </div>
+                                <div className="content">
+                                    <p className="rate">&#x20B9; {product.price}</p>
+                                    <span className="kilometer">{product.category}</span>
+                                    <p className="name">{product.name}</p>
+                                </div>
+                                <div className="date">
+                                    <span>{product.createdAt}</span>
+                                </div>
+                            </div>)
+                        })
+                    }
                 </div>
             </div>
             <div className="recommendations">
